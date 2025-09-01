d6f79dbcbf2669688c346e84b4f166f67a7f838f
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2025-09-01 10:02:19 +0300

    add-meta-data

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <h2 class="text-2xl font-semibold text-gray-800 mb-6">🛠️ Skills</h2>
-    <ul class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+    <ul class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 ceent">
       <li
         v-for="skill in skills"
         :key="skill"
-        class="flex cursor-default flex-col items-center gap-3 bg-secondary/10 hover:bg-secondary/20 transition rounded-xl p-5 shadow-sm"
+        class="flex cursor-default flex-col items-center gap-3 bg-secondary/10 hover:bg-secondary/20 transition rounded-xl p-5 shadow"
       >
         <icon :icon="skill.icon" class="w-10 h-10 object-contain" />
         <span class="font-medium text-secondary text-lg">{{ skill.name }}</span>
@@ -16,6 +16,7 @@
 
 <script setup>
 import { Icon } from "@iconify/vue";
+
 const skills = [
   { name: "HTML", icon: "devicon:html5" },
   { name: "CSS", icon: "devicon:css3" },
